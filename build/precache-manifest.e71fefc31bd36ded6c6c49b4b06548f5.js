5a14a149939276884465165e45c09d26ddeaafb5
Italo Barboza <italo_barboza@hotmail.com>
2021-01-19 01:19:56 -0300

    s

---
+++ build/precache-manifest.e71fefc31bd36ded6c6c49b4b06548f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c96c4d3ef1abbc210dfb5865585871",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f52e008e9845bea872d",
+    "url": "/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "2f52e008e9845bea872d",
+    "url": "/static/js/2.a5d63761.chunk.js"
+  },
+  {
+    "revision": "05542f73db2c31c5889e516d924a2952",
+    "url": "/static/js/2.a5d63761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df756c52bcf40cd12c69",
+    "url": "/static/js/main.0f50488b.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "cb3c3e0d86c1fc98cdf6b403ff07ff76",
+    "url": "/static/media/logo.cb3c3e0d.svg"
+  },
+  {
+    "revision": "6eac314f78fe224c4d769fb646523773",
+    "url": "/static/media/sign-in-background.6eac314f.png"
+  },
+  {
+    "revision": "72b44bdf6bfab2a9076211a1d0169cb6",
+    "url": "/static/media/validador.72b44bdf.png"
+  }
+]);
